d2656fac8ad8d832ef9224cbab714a4de49e9429
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-09-12 08:58:26 -0300

    Update bee1001.py

--- bee1001.py
+++ bee1001.py
@@ -1,11 +1,3 @@
-'''
-Esse programa lê 2 valores inteiros e armazene-os nas variáveis A e B,
-efetua a soma de A e B atribui o seu resultado na variável X.
-Imprime o fim de linha após o resultado.
-
-Autor: Luiza
-Data: 21/07/22
-'''
 #definir variáveis
 A = int(input())
 B = int(input())
