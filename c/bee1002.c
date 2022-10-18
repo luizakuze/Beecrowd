97c0ba12a51b03c4a1f7d2580f8fce016183d73d
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-10-17 21:12:27 -0300

    Create bee1002.c

---
+++ c/bee1002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define n 3.14159
+
+int main()
+{
+    double area, raio;
+
+    scanf("%lf", &raio);
+    area = n * raio * raio;
+    printf("A=%.4lf\n", area);
+
+    return 0;
+}
