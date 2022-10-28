c82b12cc5b360195cab3ce9526ce5a479e6a6b51
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-10-27 23:34:15 -0300

    Create bee1467.c

---
+++ c/bee1467.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+
+    while (scanf("%d %d %d", &A, &B, &C)!=EOF)
+    {
+    if (A == B && B == C)
+        printf("*\n");
+    else if (B == C && A != B)
+        printf("A\n");
+    else if (A == C && B != A)
+        printf("B\n");
+    else
+        printf("C\n");
+    }
+    return 0;
+}
