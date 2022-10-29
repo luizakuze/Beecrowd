29f980fea2a33c1207fa35845b0df9369e9219e2
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-10-29 14:28:14 -0300

    Update bee1020.c

--- c/bee1020.c
+++ c/bee1020.c
@@ -7,7 +7,7 @@ int main()
 
     ano = idade / 360;
     mes = (idade - 365 * ano)/30;
-	  dia = (idade - 365 * ano - 30 * mes);
+    dia = (idade - 365 * ano - 30 * mes);
 
     printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", ano, mes, dia);
     return 0;
