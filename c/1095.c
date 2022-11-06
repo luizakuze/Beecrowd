430aff0816c57c88c83d9c6b79bda00372e14d88
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-11-06 15:19:28 -0300

    Create 1095.c

---
+++ c/1095.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = -2, j = 65;
+
+    for (int k = 0; j != 0; k++) {
+        j = j - 5;
+        i = i + 3;
+        printf("I=%d J=%d\n", i, j);
+    }
+
+    return 0;
+}
