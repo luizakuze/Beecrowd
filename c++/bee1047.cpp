c5b9ef25280b612330b5483a8f28b652d9136a93
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-02-25 08:35:53 -0300

    Update and rename be1047.cpp to bee1047.cpp

--- c++/bee1047.cpp
+++ c++/bee1047.cpp
@@ -12,9 +12,13 @@ int main()
     h_total = h_final - h_inicial;
     min_total = min_final - min_inicial;
 
+    // Se a duração do jogo for negativa (o jogo termina antes de começar)
+    // 24 horas são adicionadas à duração do jogo.
     if ((h_total <= 0) && (min_total <= 0)) 
         h_total += 24;
     
+    // Se o número de minutos da duração do jogo for negativo (o jogo termina antes de começar ou dura menos de 1 hora)
+    // 60 minutos são adicionados ao total de minutos e uma hora é subtraída da duração do jogo.
     if(min_total < 0){
         min_total += 60;
         h_total--;
