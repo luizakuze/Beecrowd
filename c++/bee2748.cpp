21b3a3b79dadc6bc72e71c5d78db3989de942ec0
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-03-14 23:13:56 -0300

    Create bee2748.cpp

---
+++ c++/bee2748.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "---------------------------------------" << endl;
+    cout << "|        Roberto                      |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|        5786                         |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|        UNIFEI                       |" << endl;
+    cout << "---------------------------------------" << endl;
+
+    return 0;
+}
