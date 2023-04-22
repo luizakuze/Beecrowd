88faa8cc802450eb974c652c6969bcd284113786
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-04-21 21:36:24 -0300

    Create bee3055.cpp

---
+++ c++/bee3055.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b, m;
+
+    cin >> a;
+    cin >> m;
+
+    b = 2*m - a;
+
+    cout << b << endl;
+
+    return 0;
+}
