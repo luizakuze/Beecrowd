c9286722f2fb73bae3d6b2be37c86e2cb27b54c2
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-05-04 21:20:45 -0300

    Create bee1133.cpp

---
+++ c++/bee1133.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int X, Y;
+
+    cin >> X;
+    cin >> Y;
+
+    if (X > Y) {
+        int aux = X;
+        X = Y;
+        Y = aux;
+    }
+    
+    for (int i = X + 1; i < Y; i++)
+        if (i % 5 == 2 || i % 5 == 3)
+            cout << i << endl;
+
+    return 0;
+}
