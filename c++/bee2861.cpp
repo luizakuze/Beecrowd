5b22c298050b834414b116cd37dfb65bdb3912fa
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-05-09 10:27:09 -0300

    Create bee2861.cpp

---
+++ c++/bee2861.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    string str;
+
+    cin >> num;
+
+    while (num > 0) {
+        cin >> str;
+        cout << "gzuz" << endl;
+        num--;
+    }
+
+    return 0;
+}
